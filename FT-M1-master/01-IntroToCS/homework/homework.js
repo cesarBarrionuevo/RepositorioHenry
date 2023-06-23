0896c0b433f231a75a442c510b35a64d46d86b0c
cesarBarrionuevo <cesarbarrionuevo@cabsistemas.com.ar>
2023-06-22 21:50:11 -0300

    Solucion HomeWork

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = 0;
+   let binario = num.toString();
+   let binarioInvertido = binario.split('').reverse().join('');
+   for (let i = 0; i < binarioInvertido.length; i++) {
+      if (binarioInvertido[i] == 1) {
+         decimal += Math.pow(2, i);
+      }
+   }
+   return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let binario = '';
+   let numero = num;
+   while (numero > 0) {
+      binario += numero % 2;
+      numero = Math.floor(numero / 2);
+   }
+   return binario.split('').reverse().join('');
+}
 
 module.exports = {
    BinarioADecimal,
